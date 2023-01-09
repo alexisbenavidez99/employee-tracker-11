571b7f6a6ff00694b0645be53d054a2f78790b28
alexisbenavidez99 <callmealexis49@gmail.com>
2023-01-09 17:23:15 -0600

    added db folder

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE tracker_db;
+
+USE tracker_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT NOT NULL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id),
+    manager_id INT,
+    FOREIGN KEY (id)
+    REFERENCES employee(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO department (name)
+VALUES ("Sales"),
+       ("Human Resources"),
+       ("Legal"),
+       ("Engineering");
+
+INSERT INTO role (title, department, salary)
+VALUES ("Sales Lead", 1, 90000),
+       ("Salesperson", 1, 70000),
+       ("Trainer", 2, 65000),
+       ("HR Compliance", 2, 50000),
+       ("Lawyer", 3, 100000),
+       ("Paralegal", 3, 60000),
+       ("Senior Engineer", 4, 190000),
+       ("Software Engineer", 4, 112000);
